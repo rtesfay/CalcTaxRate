9adb327c7cf148f17bf7d0b9b260a0d68d1cfd55
rteklu <rahelteklu@gmail.com>
2021-10-04 13:08:18 -0500

    Initial commit

---
+++ src/CalcTaxRate.java
@@ -0,0 +1,45 @@
+public class CalcTaxRate {
+    private String single;
+    private String married;
+    private double taxRate;
+    private double salary;
+
+    public CalcTaxRate(){
+        this.single=single;
+        this.married=married;
+        this.taxRate=taxRate;
+        this.salary=salary;
+    }
+    public double getSalary(){
+        return salary;
+    }
+    public double TaxRate() {
+        if (salary <= 40000)
+            &&
+            if (single)
+               ||
+        if (salary <= 60000)
+            &&
+        if (married)
+                taxRate = 10 % salary;
+        System.out.println("Salary is: " +getSalary() - taxRate);
+    }
+    else
+
+    {
+        if (salary > 40000)
+            &&
+            if (single)
+            ||
+        if (salary > 60000)
+            &&
+            if (married)
+            taxRate = 18 % salary;
+        System.out.println("Salary is: " + getSalary() - taxRate);
+    }
+
+    public static void main(String[] args) {
+
+    }
+    }
+
